924e772be28ade7e4a1dd0cd454ef7f5a9bcc018
Shin Leong <sleong@wustl.edu>
2022-06-01 13:11:06 -0500

    Add GPU test scripts.

---
+++ bin/osu-test-gpu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )"
+
+if [[ $# -lt 1 ]]; then
+   echo Please add osu test.  For example "osu_gather".
+   exit 1
+fi
+export PATH="/usr/local/libexec/osu-micro-benchmarks/mpi/collective:/usr/local/libexec/osu-micro-benchmarks/mpi/one-sided:/usr/local/libexec/osu-micro-benchmarks/mpi/pt2pt:/usr/local/libexec/osu-micro-benchmarks/mpi/startup:$PATH"
+benchmark=$1
+shift
+TEST=$benchmark LSF_DOCKER_NETWORK=host LSF_DOCKER_IPC=host LSF_DOCKER_SHM_SIZE=20G bsub -q $QUEUE "$@" < $SCRIPT_DIR/osu-gpu.bsub
